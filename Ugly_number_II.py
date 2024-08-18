c23f2b8405d361b63b7a6e02de75cb9b4ac2b7a9
VarunGyanchandani <116818540+VarunGyanchandani@users.noreply.github.com>
2024-08-18 12:48:58 +0530

    Add files via upload

---
+++ Ugly_number_II.py
@@ -0,0 +1,20 @@
+def nthUglyNumber(n):
+
+    ugly = [1]
+    i2, i3, i5 = 0, 0, 0
+
+    while len(ugly) < n:
+        next_ugly = min(ugly[i2] * 2, ugly[i3] * 3, ugly[i5] * 5)
+        ugly.append(next_ugly)
+        if next_ugly == ugly[i2] * 2:
+            i2 += 1
+        if next_ugly == ugly[i3] * 3:
+            i3 += 1
+        if next_ugly == ugly[i5] * 5:
+            i5 += 1
+
+    return ugly[-1]
+
+# Example usage:
+n = 10
+print(nthUglyNumber(n))  # Output: 12
